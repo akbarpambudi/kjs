8d8f1081a95347cdaebd9b512b17b6031f5b8045
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2025-01-02 06:42:58 +0700

    WIP - feat: patching DOM

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -8,7 +8,7 @@ const createNodeSelector = {
 }
 
 
-export function mountDom(virtualDom,parentElement) {
+export function mountDom(virtualDom,parentElement,index = null) {
     if (!parentElement) {
         throw new Error('parentElement is required')
     }
@@ -25,18 +25,18 @@ export function mountDom(virtualDom,parentElement) {
 
     createNode(
         virtualDom,
-        parentElement)
+        parentElement,
+        index)
 }
 
-function createTextNode(vDom,parentEl) {
+function createTextNode(vDom,parentEl,index) {
     const {value} = vDom
     const textNode = document.createTextNode(value)
     vDom.el = textNode
-    parentEl.append(
-        textNode)
+    insert(textNode,parentEl,index)
 }
 
-function createElementNode(vDom,parentEl) {
+function createElementNode(vDom,parentEl,index) {
     const {tag,props,children} = vDom
     const el = document.createElement(tag)
 
@@ -46,15 +46,15 @@ function createElementNode(vDom,parentEl) {
 
     children.forEach(child => mountDom(child,vDom.el))
 
-    parentEl.append(el)
+    insert(el,parentEl,index)
 }
 
-function createFragmentNode(vDom,parentEl) {
+function createFragmentNode(vDom,parentEl,index) {
     const {children} = vDom
     vDom.el = parentEl
 
-    children.forEach(child => {
-        mountDom(child,vDom.el)
+    children.forEach((child,i) => {
+        mountDom(child,vDom.el,index? index+i:null)
     })
 }
 
@@ -130,4 +130,22 @@ function setAttribute(el,key,value) {
 function removeAttribute(el,key) {
     el[key] = null
     el.removeAttribute(key)
+}
+
+function insert(el,parentEl,index) {
+    if(index === null || index === undefined) {
+        parentEl.append(el)
+        return
+    }
+
+    if(index < 0) {
+        throw new Error('index must be >= 0')
+    }
+
+    const children = Array.from(parentEl.children)
+    if(index >= children.length) {
+        parentEl.append(el)
+    }else {
+        parentEl.insertBefore(el,children[index])
+    }
 }
